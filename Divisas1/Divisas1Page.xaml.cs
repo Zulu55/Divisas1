2f8a9c24c297de166db01a9c3d02c31bc2444539
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2017-05-30 12:07:50 -0500

    Creation project

---
+++ Divisas1/Divisas1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Divisas1
+{
+    public partial class Divisas1Page : ContentPage
+    {
+        public Divisas1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
